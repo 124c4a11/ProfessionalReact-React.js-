a797bc0cfd5104fb8e298024bcbd305bf3b7d0bb
124c4a11 <124c4a11@mail.ru>
2019-05-12 00:44:19 +0300

    Todo - TodoListItem: mark as important

--- 01. Todo/src/components/TodoListItem/TodoListItem.js
+++ 01. Todo/src/components/TodoListItem/TodoListItem.js
@@ -4,34 +4,37 @@ import './TodoListItem.css'
 
 
 export default class TodoListItem extends Component {
-  state = { done: false };
+  state = {
+    important: false,
+    done: false
+  };
 
   onLabelClick = () => {
     this.setState({ done: true });
   };
 
-  render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+  onMarkImportant = () => {
+    this.setState({ important: true });
+  }
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
 
     if (done) classNames += ' done';
+    if (important) classNames += ' important';
 
     return (
       <div className={classNames}>
         <span
           onClick={this.onLabelClick}
           className="todo-list-item-label"
-          style={style}
         >{ label }</span>
 
         <button
+          onClick={this.onMarkImportant}
           type="button"
           className="todo-list-item-btn btn btn-outline-success btn-sm float-right"
         ><i className="fa fa-exclamation" /></button>
