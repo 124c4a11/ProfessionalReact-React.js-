53c2bbb7bb316418df13487d96598f59e150d271
124c4a11 <124c4a11@mail.ru>
2019-05-12 00:51:42 +0300

    Todo - TodoListItem: state toggle

--- 01. Todo/src/components/TodoListItem/TodoListItem.js
+++ 01. Todo/src/components/TodoListItem/TodoListItem.js
@@ -10,12 +10,16 @@ export default class TodoListItem extends Component {
   };
 
   onLabelClick = () => {
-    this.setState({ done: true });
+    this.setState((state) => {
+      return { done: !state.done };
+    });
   };
 
   onMarkImportant = () => {
-    this.setState({ important: true });
-  }
+    this.setState((state) => {
+      return { important: !state.important };
+    });
+  };
 
   render() {
     const { label } = this.props;
