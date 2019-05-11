760ef5f47e8e79c597e84ecfc0b7cb7dba62de59
124c4a11 <124c4a11@mail.ru>
2019-05-12 00:35:01 +0300

    Todo - TodoListItem: mark as completed

--- 01. Todo/src/components/TodoListItem/TodoListItem.js
+++ 01. Todo/src/components/TodoListItem/TodoListItem.js
@@ -4,17 +4,29 @@ import './TodoListItem.css'
 
 
 export default class TodoListItem extends Component {
+  state = { done: false };
+
+  onLabelClick = () => {
+    this.setState({ done: true });
+  };
+
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
 
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
 
+    let classNames = 'todo-list-item';
+
+    if (done) classNames += ' done';
+
     return (
-      <div className="todo-list-item">
+      <div className={classNames}>
         <span
+          onClick={this.onLabelClick}
           className="todo-list-item-label"
           style={style}
         >{ label }</span>
@@ -30,5 +42,5 @@ export default class TodoListItem extends Component {
         ><i className="fa fa-trash-o" /></button>
       </div>
     );
-  }
+  };
 }
