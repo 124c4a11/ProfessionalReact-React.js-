4512e549ab6b30c03e3ff67ab6adbe1aabf400a2
124c4a11 <124c4a11@mail.ru>
2019-05-12 03:29:39 +0300

    Todo: add item

--- 01. Todo/src/components/AddForm/AddForm.js
+++ 01. Todo/src/components/AddForm/AddForm.js
@@ -4,14 +4,33 @@ import './AddForm.css';
 
 
 export default class AddForm extends Component {
+  state = { label: '' };
+
+  onLabelChange = (e) => {
+    this.setState({ label: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.onItemAdded(this.state.label);
+    this.setState({ label: '' });
+  };
+
   render() {
     return (
-      <div className="add-form">
+      <form onSubmit={this.onSubmit} className="add-form d-flex">
+        <input
+          value={this.state.label}
+          onChange={this.onLabelChange}
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+        />
         <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded('Add Item')}
+          className="btn btn-outline-secondary flex-shrink-0 ml-1"
         >Add Item</button>
-      </div>
+      </form>
     );
   };
 };
